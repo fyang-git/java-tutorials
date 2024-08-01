54438f76733bc74346d7afa5df88c4e2cb31bada
fyang-git <fyang@42mail.sutd.edu.sg>
2024-08-01 08:03:10 +0800

    Add files via upload

---
+++ ICT167Lab3.1/src/Fraction.java
@@ -0,0 +1,16 @@
+public class Fraction {
+    public int numerator;
+    public int denominator;
+
+    public Fraction(int num, int denom) {
+        if (denom == 0) {
+            throw new IllegalArgumentException("Denominator cannot be zero.");
+        }
+        this.numerator = num;
+        this.denominator = denom;
+    }
+
+    public void printFraction() {
+        System.out.println(this.numerator + " / " + this.denominator);
+    }
+}
---
+++ ICT167Lab3.1/src/TestFraction.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class TestFraction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter the numerator (negative to quit): ");
+            int numerator = scanner.nextInt();
+
+            if (numerator < 0) {
+                System.out.println("Exiting program.");
+                break;
+            }
+
+            System.out.print("Enter the denominator: ");
+            int denominator = scanner.nextInt();
+
+            if (denominator == 0) {
+                System.out.println("The denominator cannot be zero. Please try again.");
+                continue;
+            }
+
+            Fraction fraction = new Fraction(numerator, denominator);
+            System.out.print("The fraction is: ");
+            fraction.printFraction();
+        }
+        scanner.close();
+    }
+}
